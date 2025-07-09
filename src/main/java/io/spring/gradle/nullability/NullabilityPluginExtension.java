9a418986c6ec2245647bff2188dcdb60b0a77456
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-07-09 21:26:24 +0100

    Upgrade to Error Prone 2.40.0
    Closes gh-3

--- src/main/java/io/spring/gradle/nullability/NullabilityPluginExtension.java
+++ src/main/java/io/spring/gradle/nullability/NullabilityPluginExtension.java
@@ -25,7 +25,7 @@ import org.gradle.api.provider.Property;
  */
 public abstract class NullabilityPluginExtension {
 
-	static final String ERROR_PRONE_VERSION = "2.39.0";
+	static final String ERROR_PRONE_VERSION = "2.40.0";
 
 	static final String NULL_AWAY_VERSION = "0.12.7";
 
