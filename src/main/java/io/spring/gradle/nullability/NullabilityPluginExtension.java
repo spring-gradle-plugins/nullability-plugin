a221431540154eee9837909b438880984dfad2f3
Andy Wilkinson <andy.wilkinson@broadcom.com>
2026-04-30 13:19:21 +0100

    Upgrade to NullAway 0.13.4
    Closes gh-41

--- src/main/java/io/spring/gradle/nullability/NullabilityPluginExtension.java
+++ src/main/java/io/spring/gradle/nullability/NullabilityPluginExtension.java
@@ -27,7 +27,7 @@ public abstract class NullabilityPluginExtension {
 
 	static final String ERROR_PRONE_VERSION = "2.49.0";
 
-	static final String NULL_AWAY_VERSION = "0.13.2";
+	static final String NULL_AWAY_VERSION = "0.13.4";
 
 	/**
 	 * Internal use only.
